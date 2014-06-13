302ba00b0e2b443b669e05cfffeba91bb1428b4f
Benjamin Arbogast <benjamin.arbogast@gmail.com>
2014-06-13 17:03:33 +0200

    Implemented custom views

--- model.py
+++ model.py
@@ -44,6 +44,15 @@ class Game(db.Model):
     def __repr__(self): return '%s - %s' % (self.team_a.name, self.team_b.name)
     
     
+    def repr_score(self):
+        if self.team_a_score is not None:
+            return '{0.team_a_score} : {0.team_b_score}'.format(self)
+        else:
+            return ''
+
+
+
+
 class Tipper(db.Model):
     id =  db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False, unique=True)
@@ -64,6 +73,21 @@ class Tipp(db.Model):
     
     def __repr__(self): return '%s: %s - %s' % (self.game.__repr__(), self.team_a_tipp, self.team_b_tipp)
 
+    def get_score(self):
+        if self.game.team_a_score is None:
+            return None
+
+        elif self.game.team_a_score == self.team_a_tipp and \
+             self.game.team_b_score == self.team_b_tipp:
+            return 3
+
+        elif (self.game.team_a_score > self.game.team_b_score and self.team_a_tipp > self.team_b_tipp) or \
+             (self.game.team_a_score < self.game.team_b_score and self.team_a_tipp < self.team_b_tipp):
+            return 1
+
+        else:
+            return 0
+
 
 
 if __name__ == '__main__':
@@ -71,7 +95,7 @@ if __name__ == '__main__':
     from flask.ext.admin.contrib.sqla import ModelView
     
 
-    from flask import Flask
+    from flask import Flask, render_template
 
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
@@ -102,7 +126,70 @@ if __name__ == '__main__':
     admin.add_view(ModelView(Game, session))
     admin.add_view(ModelView(Tipper, session))
     admin.add_view(ModelView(Tipp, session))
-    
+
+
+    @app.route('/teams')
+    def teams():
+        return render_template('teams.html', groups=Group.query.all())
+
+    @app.route('/games')
+    def games():
+        return render_template('games.html', games=Game.query.all())
+
+    @app.route('/tippers')
+    def tippers():
+        res = db.engine.execute('''
+            select
+                t.id,
+                t.name,
+                sum(
+                    case
+                        when ti.team_a_tipp = g.team_a_score and ti.team_b_tipp = g.team_b_score then 3
+                        when
+                            (g.team_a_score > g.team_b_score and ti.team_a_tipp > ti.team_b_tipp) or
+                            (g.team_a_score < g.team_b_score and ti.team_a_tipp < ti.team_b_tipp)
+                        then 1
+                        else 0 end
+                )as score
+            from tipper t
+            join tipp ti on t.id = ti.tipper_id
+            join game g on ti.game_id = g.id
+            where g.team_a_score is not null
+            group by t.id
+        ''')
+        return render_template('tippers.html', tippers=res)
+
+    @app.route('/team/<int:team_id>')
+    def team(team_id):
+        return render_template('team.html', team=Team.query.get(team_id))
+
+    @app.route('/game/<int:game_id>')
+    def game(game_id):
+        return render_template('game.html', game=Game.query.get(game_id))
+
+    @app.route('/tipper/<int:tipper_id>')
+    def tipper(tipper_id):
+        ((total_score, ), ) = db.engine.execute('''select
+                sum(
+                    case
+                        when ti.team_a_tipp = g.team_a_score and ti.team_b_tipp = g.team_b_score then 3
+                        when
+                            (g.team_a_score > g.team_b_score and ti.team_a_tipp > ti.team_b_tipp) or
+                            (g.team_a_score < g.team_b_score and ti.team_a_tipp < ti.team_b_tipp)
+                        then 1
+                        else 0 end
+                )as score
+            from tipper t
+            join tipp ti on t.id = ti.tipper_id
+            join game g on ti.game_id = g.id
+            where
+                g.team_a_score is not null and
+                t.id = :tipper_id
+            group by t.id
+        ''', tipper_id=tipper_id)
+
+        return render_template('tipper.html', tipper=Tipper.query.get(tipper_id), total_score=total_score)
+
     app.run(port=5555)
     
     
