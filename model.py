3cb1cda9ee3318e66cfb3034f983630b412b58e4
Benjamin Arbogast <benjamin.arbogast@gmail.com>
2014-06-14 21:40:49 +0200

    Export app as module attribute

--- model.py
+++ model.py
@@ -90,111 +90,109 @@ class Tipp(db.Model):
 
 
 
+from flask.ext.admin import Admin
+from flask.ext.admin.contrib.sqla import ModelView
+
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+app.config['SQLALCHEMY_ECHO'] = False
+app.secret_key = 'Todo'
+
+db.init_app(app)
+with app.test_request_context():
+    db.create_all(app=app)
+
+    group_a = Group(name='A')
+    halbfinale = GameType(name='Halbfinale')
+    deutschland = Team(name='Deutschland', group=group_a)
+    england = Team(name='England', group=group_a)
+    ben = Tipper(name='Ben')
+    sarina = Tipper(name='Sarina')
+
+    #db.session.add_all([group_a, halbfinale, deutschland, england, ben, sarina])
+    db.session.commit()
+
+session = db.session
+
+admin = Admin(app)
+admin.add_view(ModelView(Group, session))
+admin.add_view(ModelView(Team, session))
+admin.add_view(ModelView(GameType, session))
+admin.add_view(ModelView(Game, session))
+admin.add_view(ModelView(Tipper, session))
+admin.add_view(ModelView(Tipp, session))
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/teams')
+def teams():
+    return render_template('teams.html', groups=Group.query.all())
+
+@app.route('/games')
+def games():
+    return render_template('games.html', games=Game.query.all())
+
+@app.route('/tippers')
+def tippers():
+    res = db.engine.execute('''
+        select
+            t.id,
+            t.name,
+            sum(
+                case
+                    when ti.team_a_tipp = g.team_a_score and ti.team_b_tipp = g.team_b_score then 3
+                    when
+                        (g.team_a_score > g.team_b_score and ti.team_a_tipp > ti.team_b_tipp) or
+                        (g.team_a_score < g.team_b_score and ti.team_a_tipp < ti.team_b_tipp)
+                    then 1
+                    else 0 end
+            )as score
+        from tipper t
+        join tipp ti on t.id = ti.tipper_id
+        join game g on ti.game_id = g.id
+        where g.team_a_score is not null
+        group by t.id
+    ''')
+    return render_template('tippers.html', tippers=res)
+
+@app.route('/team/view/<int:team_id>')
+def team(team_id):
+    return render_template('team.html', team=Team.query.get(team_id))
+
+@app.route('/game/view/<int:game_id>')
+def game(game_id):
+    return render_template('game.html', game=Game.query.get(game_id))
+
+@app.route('/tipper/view/<int:tipper_id>')
+def tipper(tipper_id):
+    ((total_score, ), ) = db.engine.execute('''select
+            sum(
+                case
+                    when ti.team_a_tipp = g.team_a_score and ti.team_b_tipp = g.team_b_score then 3
+                    when
+                        (g.team_a_score > g.team_b_score and ti.team_a_tipp > ti.team_b_tipp) or
+                        (g.team_a_score < g.team_b_score and ti.team_a_tipp < ti.team_b_tipp)
+                    then 1
+                    else 0 end
+            )as score
+        from tipper t
+        join tipp ti on t.id = ti.tipper_id
+        join game g on ti.game_id = g.id
+        where
+            g.team_a_score is not null and
+            t.id = :tipper_id
+        group by t.id
+    ''', tipper_id=tipper_id)
+
+    return render_template('tipper.html', tipper=Tipper.query.get(tipper_id), total_score=total_score)
+
+
 if __name__ == '__main__':
-    from flask.ext.admin import Admin
-    from flask.ext.admin.contrib.sqla import ModelView
-
-
-    from flask import Flask, render_template
-
-    app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
-    app.config['SQLALCHEMY_ECHO'] = False
     app.debug = True
-    app.secret_key = 'Todo'
-
-    db.init_app(app)
-    with app.test_request_context():
-        db.create_all(app=app)
-
-        group_a = Group(name='A')
-        halbfinale = GameType(name='Halbfinale')
-        deutschland = Team(name='Deutschland', group=group_a)
-        england = Team(name='England', group=group_a)
-        ben = Tipper(name='Ben')
-        sarina = Tipper(name='Sarina')
-
-        #db.session.add_all([group_a, halbfinale, deutschland, england, ben, sarina])
-        db.session.commit()
-
-    session = db.session
-
-    admin = Admin(app)
-    admin.add_view(ModelView(Group, session))
-    admin.add_view(ModelView(Team, session))
-    admin.add_view(ModelView(GameType, session))
-    admin.add_view(ModelView(Game, session))
-    admin.add_view(ModelView(Tipper, session))
-    admin.add_view(ModelView(Tipp, session))
-
-
-    @app.route('/')
-    def index():
-        return render_template('index.html')
-
-    @app.route('/teams')
-    def teams():
-        return render_template('teams.html', groups=Group.query.all())
-
-    @app.route('/games')
-    def games():
-        return render_template('games.html', games=Game.query.all())
-
-    @app.route('/tippers')
-    def tippers():
-        res = db.engine.execute('''
-            select
-                t.id,
-                t.name,
-                sum(
-                    case
-                        when ti.team_a_tipp = g.team_a_score and ti.team_b_tipp = g.team_b_score then 3
-                        when
-                            (g.team_a_score > g.team_b_score and ti.team_a_tipp > ti.team_b_tipp) or
-                            (g.team_a_score < g.team_b_score and ti.team_a_tipp < ti.team_b_tipp)
-                        then 1
-                        else 0 end
-                )as score
-            from tipper t
-            join tipp ti on t.id = ti.tipper_id
-            join game g on ti.game_id = g.id
-            where g.team_a_score is not null
-            group by t.id
-        ''')
-        return render_template('tippers.html', tippers=res)
-
-    @app.route('/team/view/<int:team_id>')
-    def team(team_id):
-        return render_template('team.html', team=Team.query.get(team_id))
-
-    @app.route('/game/view/<int:game_id>')
-    def game(game_id):
-        return render_template('game.html', game=Game.query.get(game_id))
-
-    @app.route('/tipper/view/<int:tipper_id>')
-    def tipper(tipper_id):
-        ((total_score, ), ) = db.engine.execute('''select
-                sum(
-                    case
-                        when ti.team_a_tipp = g.team_a_score and ti.team_b_tipp = g.team_b_score then 3
-                        when
-                            (g.team_a_score > g.team_b_score and ti.team_a_tipp > ti.team_b_tipp) or
-                            (g.team_a_score < g.team_b_score and ti.team_a_tipp < ti.team_b_tipp)
-                        then 1
-                        else 0 end
-                )as score
-            from tipper t
-            join tipp ti on t.id = ti.tipper_id
-            join game g on ti.game_id = g.id
-            where
-                g.team_a_score is not null and
-                t.id = :tipper_id
-            group by t.id
-        ''', tipper_id=tipper_id)
-
-        return render_template('tipper.html', tipper=Tipper.query.get(tipper_id), total_score=total_score)
-
     app.run(port=5555)
-
-
-
